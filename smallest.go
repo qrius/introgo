9bbb98db772d226761d5528b273e5af4bd757bb4
Luke Lam <qriusim+github@gmail.com>
2018-10-13 23:34:04 -0700

    smallest.go initial commit

---
+++ smallest.go
@@ -0,0 +1,17 @@
+/*
+Package smallest finds the smallest number in this list:
+
+x :=[] int{
+    48,96,86,68,
+    57,82,63,70,
+    37,34,83,27,
+    19,97, 9,17,
+}
+*/
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello")
+}
