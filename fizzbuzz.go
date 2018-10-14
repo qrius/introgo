76bc9fcfd957b3f98745943f1750f02edcc27888
Luke Lam <qriusim+github@gmail.com>
2018-10-13 22:22:14 -0700

    change range start from 0 to 1

--- fizzbuzz.go
+++ fizzbuzz.go
@@ -16,7 +16,7 @@ package main
 import "fmt"
 
 func main() {
-	for i := 0; i < 100; i++ {
+	for i := 1; i < 100; i++ {
 		if (i%3 == 0) && (i%5 == 0) {
 			fmt.Printf("%s ", "FizzBuzz")
 		} else if (i % 3) == 0 {
