d48505229ff4fb0994852f3e53d5a529c9204d5e
Luke Lam <qriusim+github@gmail.com>
2018-10-16 07:14:21 -0700

    makeOddGenerator complete

--- makeOddGenerator.go
+++ makeOddGenerator.go
@@ -4,18 +4,20 @@ package makeOddGenerator returns odd numbers
 
 package main
 
+import "fmt"
+
 func makeOddGenerator() func() uint {
-	i := uint(0)
+	i := uint(1)
 	return func() (ret uint) {
 		ret = i
-		i += 1
+		i += 2
 		return
 	}
 }
 
 func main() {
 	nextOdd := makeOddGenerator()
-	fm.Println(nextOdd())
-	fm.Println(nextOdd())
-	fm.Println(nextOdd())
+	fmt.Println(nextOdd())
+	fmt.Println(nextOdd())
+	fmt.Println(nextOdd())
 }
