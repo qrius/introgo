3c0a3d46411fc506a1947fd5abbebca4e6d4b0ef
Luke Lam <qriusim+github@gmail.com>
2018-10-23 13:42:56 -0700

    example of pointer usage

--- swap.go
+++ swap.go
@@ -3,9 +3,13 @@ package main
 import "fmt"
 
 func swap(xPtr *int, yPtr *int) {
-	c := yPtr
-	yPtr = xPtr
-	xPtr = c
+	println("parameter y:", yPtr)
+	println("address of yPtr: ", &yPtr)
+	println("content of *yPtr: ", *yPtr)
+	//set c to the content at the address dereferenced by *yPtr
+	c := *yPtr
+	*yPtr = *xPtr
+	*xPtr = c
 }
 
 func main() {
@@ -13,6 +17,7 @@ func main() {
 	y := 2
 	fmt.Println("x = ", x)
 	fmt.Println("y = ", y)
+	println("address of y: ", &y)
 
 	swap(&x, &y)
 
