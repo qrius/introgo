cd91678aca90f988dd4fd458b067142c8cfdd472
Luke Lam <qriusim+github@gmail.com>
2018-10-28 17:16:52 -0700

    completed example project Deck of cards

---
+++ Cards/main.go
@@ -0,0 +1,19 @@
+package main
+
+func main() {
+
+	// cards := newDeck()
+	// hand, remainingCards := deal(cards, 5)
+
+	// hand.print()
+	// remainingCards.print()
+	// cards := newDeck()
+	// fmt.Println(cards.toString())
+	// cards.saveToFile("my_cards.txt")
+	//cards := newDeckFromFile("my_cards.")
+	//cards.print()
+
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
+}
