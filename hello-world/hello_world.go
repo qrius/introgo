a9c58a6c2cd9d9180040802be9443cd3e3ba4d6b
Luke Lam <qriusim+github@gmail.com>
2018-11-18 00:27:03 -0800

    update to Hello

---
+++ hello-world/hello_world.go
@@ -0,0 +1,7 @@
+//"Package greeting is the shortest package to test package setup"
+package greeting
+
+// HelloWorld function to test environment setup
+func HelloWorld() string {
+	return "Hello, World!"
+}
