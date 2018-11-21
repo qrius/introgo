e0ef9aac9b900bf77720fbf35f0458751bc26ea4
Luke Lam <qriusim+github@gmail.com>
2018-11-21 09:13:00 -0800

    two_fer.go refactored out unnecessary variables

--- two-fer/two_fer.go
+++ two-fer/two_fer.go
@@ -7,23 +7,10 @@ import (
 
 // ShareWith function returns a simple count for sharing scheme
 func ShareWith(name string) string {
-	var v string
-	// var b bytes.Buffer
 
-	l := len(name)
-
-	switch {
-	case l == 0:
-		v = "you"
-	case l > 0:
-		v = name
-	default:
-		v = "string not recognizable"
+	if len(name) == 0 {
+		name = "you"
 	}
 
-	// b.WriteString("One for ")
-	// b.WriteString(v)
-	// b.WriteString(", one for me.")
-	// return (b.String())
-	return (fmt.Sprintf("%s%s%s", "One for ", v, ", one for me."))
+	return fmt.Sprintf("%s%s%s", "One for ", name, ", one for me.")
 }
