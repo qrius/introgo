0f68f2cb6cb44a6c8c2b04389ec9e1a7d0f47ff5
Luke Lam <qriusim+github@gmail.com>
2018-12-02 23:11:07 -0800

    worked on triangle exercise

---
+++ triangle/triangle.go
@@ -0,0 +1,39 @@
+// Package triangle should have a package comment that summarizes what it's about.
+// https://golang.org/doc/effective_go.html#commentary
+package triangle
+
+// Kind type represent triangle types.  KindFromSides() returns this type.
+type Kind int
+
+const (
+	//NaT = not triangle
+	NaT Kind = iota // 0
+	//Equ = equilateral
+	Equ // 1
+	//Iso = isosceles
+	Iso // 2
+	//Sca = scalene
+	Sca // 3
+)
+
+// KindFromSides should have a comment documenting it.
+func KindFromSides(a, b, c float64) Kind {
+
+	var k Kind
+
+	if a == b && a == c {
+		k = Equ
+	} else {
+		if a == b || b == c || a == c {
+			k = Iso
+		} else {
+			k = Sca
+		}
+	}
+
+	if (a <= 0) || (b <= 0) || (c <= 0) || !(a+b == c) || !(b+c == a) || !(a+c == b) {
+		k = NaT
+	}
+
+	return k
+}
